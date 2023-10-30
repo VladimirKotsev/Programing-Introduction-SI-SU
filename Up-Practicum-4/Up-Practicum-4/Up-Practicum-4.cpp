a1e3070b2ad479544542b2711319243305142c95
VladimirKotsev <v.kotsev04@gmail.com>
2023-10-30 21:30:49 +0200

    Adding all the solutions + homework

--- Up-Practicum-4/Up-Practicum-4/Up-Practicum-4.cpp
+++ Up-Practicum-4/Up-Practicum-4/Up-Practicum-4.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -16,20 +18,190 @@ int IsSumN(int n)
 	else { return 0; }
 }
 
-int main()
+char toLower(char ch)
 {
-	//Зад. 8
-	bool IsEqualToSum = false;
-	int n = 0;
+	return (char)((int)ch + 32);
+}
+char toUpper(char ch)
+{
+	return (char)((int)ch - 32);
+}
 
-	while (!IsEqualToSum)
+string concatInt(int n, int m)
+{
+	return to_string(n) + to_string(m);
+}
+
+int sumOfPrimeNums(int start, int end)
+{
+	int sumOfDividers = 0;
+
+	for (int i = start; i <= end; i++)
 	{
-		cin >> n;
+		bool isPrime = true;
+		for (int j = 2; j < i; j++)
+		{
+			if (i % j == 0 && j != 1)
+			{
+				isPrime = false;
+				break;
+			}
+		}
 
+		if (isPrime)
+		{
+			sumOfDividers += i;
+		}
 	}
 
+	return sumOfDividers;
+}
+
+int toSqrt(int n)
+{
+	return floor(sqrt(n));
+}
+
+int sumOfNums(int n)
+{
+	int sum = 0;
+
+	while (n != 0)
+	{
+		int digit = n % 10;
+		n /= 10;
+
+		sum += digit;
+	}
+
+	return sum;
+}
+
+bool isSorted(int n)
+{
+	int digit = 0;
+	int previousDigit = 0;
+
+	while (n != 0)
+	{
+		digit = n % 10;
+		n /= 10;
+
+		if (previousDigit == 0)
+		{
+			previousDigit = digit;
+			continue;
+		}
+
+		if (previousDigit <= digit)
+		{
+			return false;
+		}
+
+		previousDigit = digit;
+	}
+
+	return true;
+}
+
+void enterSumOfPrevious()
+{
+	int n = -1;
+	int sum = 1;
+
+	while (n != sum)
+	{
+		sum += n;
+		cin >> n;
+	}
+}
+
+bool isRowEvenOrOdd(int n)
+{
+	int previous = 1;
+	int current = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		current = previous * 3 / 2;
+		previous = current;
+	}
+
+	if (current % 2 == 0)
+	{ return true; }
+	else 
+	{ return false; }
+}
+
+int main()
+{
+	//Зад. 1
+	/*char c;
+	cin >> c;
+
+	if ((int)c >= 65 && (int)c <= 90)
+	{
+		c = toLower(c);
+		cout << (char)c;
+	}
+	else if ((int)c >= 97 && (int)c <= 122)
+	{
+		c = toUpper(c);
+		cout << (char)c;
+	}
+	else
+	{
+		cout << "Not a letter!";
+		return 0;
+	}*/
+
+	//Зад. 3
+	/*int firstNumber;
+	int secondNumber;
+
+	cin >> firstNumber >> secondNumber;
+
+	string concatNumber = concatInt(firstNumber, secondNumber);
+
+	cout << concatNumber;*/
+
+	//Зад. 4
+	/*int start = 0;
+	int end = 0;
+	int sumOfDividers = 0;
+
+	cin >> start >> end;
+
+	cout << sumOfPrimeNums(start, end);*/
+
+	//Зад. 5
+	/*int n = 0;
+	cin >> n;
+
+	cout << toSqrt(n);*/
+
+	//Зад. 6
+	/*int n = 0;
+	cin >> n;
+
+	cout << sumOfNums(n);*/
+
+	//Зад 7.
+	/*int n = 0;
+	cin >> n;
+
+	bool check = isSorted(n);
+	if (check)
+	{ cout << "True!"; }
+	else 
+	{ cout << "False!"; }*/
+
+	//Зад. 8
+	/*enterSumOfPrevious();
+	return 0;*/
+
 	//Зад. 9
-	int n = 0;
+	/*int n = 0;
 	cin >> n;
 
 	for (int i = 0; i < n; i++)
@@ -39,8 +211,15 @@ int main()
 			cout << "Yes ";
 		}
 		else { cout << "No "; }
-	}
+	}*/
 
 	//Зад. 10
+	/*int n = 0;
+	cin >> n;
 
+	if (isRowEvenOrOdd)
+	{ cout << "Even!"; }
+	else 
+	{ cout << "Odd!"; }*/
 }
+
