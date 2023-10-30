c248c0eed9e87066616a43cd3f89bbfcd34d22aa
VladimirKotsev <v.kotsev04@gmail.com>
2023-10-30 19:15:11 +0200

    Adding Practicum 4 folder

---
+++ Up-Practicum-4/Up-Practicum-4/Up-Practicum-4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int IsSumN(int n)
+{
+	int firstNumber = 0;
+	int secondNumber = 0;
+
+	cin >> firstNumber >> secondNumber;
+
+	if (firstNumber + secondNumber == n)
+	{
+		return 1;
+	}
+	else { return 0; }
+}
+
+int main()
+{
+	//Зад. 8
+	bool IsEqualToSum = false;
+	int n = 0;
+
+	while (!IsEqualToSum)
+	{
+		cin >> n;
+
+	}
+
+	//Зад. 9
+	int n = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (IsSumN(n) == 1)
+		{
+			cout << "Yes ";
+		}
+		else { cout << "No "; }
+	}
+
+	//Зад. 10
+
+}
